12078efaaa63a21d59267ee5819a359f6541b5d2
clairtonluz <clairton.c.l@gmail.com>
2017-07-31 11:06:14 -0300

    centralizando informações de configuração de ambiente

--- src/main/java/br/com/clairtonluz/sicoba/config/security/JWTAuthenticationFilter.java
+++ src/main/java/br/com/clairtonluz/sicoba/config/security/JWTAuthenticationFilter.java
@@ -6,10 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -24,8 +21,6 @@ import java.io.IOException;
  * Created by clairton on 04/06/17.
  */
 public class JWTAuthenticationFilter extends GenericFilterBean {
-    private AuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-    private AuthenticationFailureHandler failureHandler = new SimpleUrlAuthenticationFailureHandler();
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
@@ -45,30 +40,26 @@ public class JWTAuthenticationFilter extends GenericFilterBean {
     }
 
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
-        System.out.println("attemptAuthentication");
-        Authentication authentication = null;
         try {
-            authentication = TokenAuthenticationService.getAuthentication(request);
+            Authentication authentication = TokenAuthenticationService.getAuthentication(request);
             if (authentication == null) {
                 throw new BadCredentialsException("Nenhuma usuário encontrado, faça o login com um usuário existente");
             }
+            return authentication;
         } catch (ExpiredJwtException e) {
             throw new BadCredentialsException("Sua sessão expirou faça o login novamente", e);
         } catch (SignatureException e) {
             throw new BadCredentialsException("Credenciais Inválida, por favor faça o login", e);
         }
-        return authentication;
     }
 
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        System.out.println("successfulAuthentication");
+        SecurityContextHolder.getContext().setAuthentication(authResult);
         chain.doFilter(request, response);
-//        response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, Constants.ALLOW_ORIGIN);
-//        successHandler.onAuthenticationSuccess(request, response, authResult);
     }
 
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-        System.out.println("unsuccessfulAuthentication");
-        failureHandler.onAuthenticationFailure(request, response, failed);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                "Falha na authenticação: " + failed.getMessage());
     }
 }
